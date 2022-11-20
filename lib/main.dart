bb07efee838619dfce086e9c0efe7c803b98d6aa
suyash-jathar <suyashjathar15@gmail.com>
2022-11-20 10:37:08 +0530

    Added Inherited_Notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( 
-    MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    )
-  );
+  runApp(MaterialApp(
+    title: "Flutter Demo",
+    theme: ThemeData(primarySwatch: Colors.blue),
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatefulWidget {
@@ -20,6 +16,52 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class SliderData extends ChangeNotifier {
+  double _value = 0.0;
+  double get value => _value;
+  set value(double newValue) {
+    if (newValue != value) {
+      _value = newValue;
+      // IMp
+      notifyListeners();
+    }
+  }
+}
+
+// Defining it Globally
+final sliderdata = SliderData();
+
+// Created an instance of inherited notifier .It gonna rebuild child whenever it calls notifylistener
+// ChangeNotifier hold on to the state - Stays on Top
+// Inherited-Notifier -> 
+//                      - rebuilds the child 
+//                      - Stays at bottom 
+//                      - listens to change ChangeNotifier and build child 
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier({
+    Key? key,
+    required SliderData sliderData,
+    required Widget child,
+  }) : super(
+          key:key,
+          notifier: sliderData,
+          child: child,
+        );
+
+
+        // It gonna return BuildContext 
+        // 1. Frst Grab the SliderNotifier 
+        //    if it is present then check for notifyListener() and grab it  
+        //    if it is present then check for value and grab it . 
+      static double of(BuildContext context) =>
+        context.
+                dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
+                ?.notifier
+                ?.value
+                ?? 0.0;
+
+}
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
@@ -27,7 +69,51 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
+      body: SliderInheritedNotifier(
+        sliderData: sliderdata,
+        // Builder is used to allow the fresh part of context 
+        child: Builder(
+          builder: (context) {
+            return Column(
+              children: [
+                Slider(
+                  value:  SliderInheritedNotifier.of(context), 
+                  onChanged: (value) {
+                    sliderdata.value=value;
+                  }
+                  ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(context),
+                      child: Container(
+                        color: Colors.yellow,
+                        height: 200,
+                      ),
+                    ),
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(context),
+                      child: Container(
+                        color: Colors.blue,
+                        height: 200,
+                      ),
+                    ),
+                  ].expandEqually().toList(),
+                )
+              ],
+            );
+          }
+        ),
+      ),
     );
   }
 }
 
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map((e) {
+        return Expanded(
+          child: e,
+        );
+      });
+}
